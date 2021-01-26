d05b9fbd72d3fc03e3491acbe8d670edd4d40aac
Stephanie La Raja <stephanielaraja@gmail.com>
2021-01-26 11:56:55 -0500

    commit tree aa7fafbbdf64f5abff75abef948b552f793fdce5

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+    attr_accessor :word 
+    def match(list_of_words)
+        
+        original_array = list_of_words
+        new_array = original_array
+       
+        final_array = []
+        new_array.each_with_index do |phrase,index|
+          word_to_check = phrase.split("")
+          word_to_check.sort
+          
+          if @word.split("").sort == word_to_check.sort
+            final_array << original_array[index]
+          end
+        end
+        return final_array
+      end
+end
